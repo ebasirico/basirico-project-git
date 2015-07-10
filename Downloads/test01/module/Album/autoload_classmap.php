551d562aaaa31ad1300df3fe67272ce2aa6362f2
Enrico Basiricò <enri.basi97@gmail.com>
2015-07-10 11:44:09 +0200

    Eliminazione Cartella Download

--- Downloads/test01/module/Album/autoload_classmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Enrico
- * Date: 09/06/2015
- * Time: 14:55
- */
-
-return array();
--- Downloads/test01/module/Wator/autoload_classmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Enrico
- * Date: 10/06/2015
- * Time: 11:01
- */
-
-return array();
